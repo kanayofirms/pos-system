556e3903f4874979eced121683980ad1ca1d7f3a
kanayofirms <kanyeze@gmail.com>
2024-12-23 04:35:35 +0100

    feat(routes): add admin and user dashboard routes with middleware groups

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', [AuthController::class, 'login']);
 Route::post('login_post', [AuthController::class, 'login_post']);
+
+Route::group(['middleware' => 'admin'], function () {
+    Route::get('admin/dashboard', [DashboardController::class, 'dashboard']);
+});
+
+Route::group(['middleware' => 'user'], function () {
+    Route::get('user/dashboard', [DashboardController::class, 'dashboard']);
+});
