6df37571e90879104777d553e69d2c46b1abd754
kanayofirms <kanyeze@gmail.com>
2024-12-23 04:07:03 +0100

    feat(routes): add POST route for login_post

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::get('/', [AuthController::class, 'login']);
+Route::post('login_post', [AuthController::class, 'login_post']);
