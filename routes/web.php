793b568e21be051d0f7ccbee2134934bfea6f6f7
kanayofirms <kanyeze@gmail.com>
2024-12-28 10:54:53 +0100

    feat(routes): add route for admin category index page

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', [AuthController::class, 'login']);
 Route::post('login_post', [AuthController::class, 'login_post']);
 
 Route::group(['middleware' => 'admin'], function () {
     Route::get('admin/dashboard', [DashboardController::class, 'dashboard']);
+    Route::get('admin/category', [CategoryController::class, 'index']);
 });
 
 Route::group(['middleware' => 'user'], function () {
