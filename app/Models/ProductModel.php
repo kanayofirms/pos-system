930431c863cd9c5dec626dd8d2ed7e463a690e10
kanayofirms <kanyeze@gmail.com>
2024-12-31 16:53:21 +0100

    feat(ProductModel): create product model with fillable attributes

---
+++ app/Models/ProductModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Request;
+
+class ProductModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'product';
+
+    protected $fillable = [
+        'category_id',
+        'product_code',
+        'name_product',
+        'brand',
+        'purchase_price',
+        'selling_price',
+        'discount',
+        'stock',
+    ];
+}
