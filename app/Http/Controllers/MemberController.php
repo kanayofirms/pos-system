b21496868a64800db9727d932ba20348f8321d22
kanayofirms <kanyeze@gmail.com>
2025-01-04 05:45:20 +0100

    feat(member): add store method to save new member details

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -16,4 +16,15 @@ class MemberController extends Controller
     {
         return view('member.add');
     }
+
+    public function store(Request $request)
+    {
+        $save = new MemberModel;
+        $save->name_member = trim($request->name_member);
+        $save->address = trim($request->address);
+        $save->telephone = trim($request->telephone);
+        $save->save();
+
+        return redirect('admin/member')->with('success', "Member successfully created.");
+    }
 }
