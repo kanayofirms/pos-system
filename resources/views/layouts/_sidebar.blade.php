7bc2a21107ec466e47dd3d1119b52e5d633b7d96
kanayofirms <kanyeze@gmail.com>
2025-01-07 18:32:07 +0100

    refactor(view): update image source path for AdminLTE logo

--- resources/views/layouts/_sidebar.blade.php
+++ resources/views/layouts/_sidebar.blade.php
@@ -1,7 +1,7 @@
 <!--begin::Sidebar-->
 <aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark"> <!--begin::Sidebar Brand-->
     <div class="sidebar-brand"> <!--begin::Brand Link--> <a href="./index.html" class="brand-link">
-            <!--begin::Brand Image--> <img src="../../dist/assets/img/AdminLTELogo.png" alt="AdminLTE Logo"
+            <!--begin::Brand Image--> <img src="{{ asset('dist/assets/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
                 class="brand-image opacity-75 shadow"> <!--end::Brand Image--> <!--begin::Brand Text--> <span
                 class="brand-text fw-light">Pembo POS</span> <!--end::Brand Text--> </a>
         <!--end::Brand Link-->
