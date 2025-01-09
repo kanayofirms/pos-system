a22e8b8601cdc72bb3d473ccc83964d3e015a422
kanayofirms <kanyeze@gmail.com>
2025-01-09 10:42:15 +0100

    feat(controller): add index method to ExpenseController for displaying expense list view

---
+++ app/Http/Controllers/ExpenseController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\ExpenseModel;
+
+class ExpenseController extends Controller
+{
+    public function index()
+    {
+        return view('expense.list');
+    }
+}
