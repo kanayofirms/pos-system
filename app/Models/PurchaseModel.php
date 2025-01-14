5adab1a3e25ba0691cfe386856817014f3b18ab0
kanayofirms <kanyeze@gmail.com>
2025-01-14 14:50:34 +0100

    feat(model): add search filters to getRecord method for purchases

--- app/Models/PurchaseModel.php
+++ app/Models/PurchaseModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Request;
 
 class PurchaseModel extends Model
 {
@@ -16,6 +17,28 @@ class PurchaseModel extends Model
         $return = self::select('purchase.*', 'supplier.supplier_name')
             ->join('supplier', 'purchase.supplier_id', '=', 'supplier.id')
             ->orderBy('id', 'desc');
+
+        // Search start
+        if (!empty(Request::get('id'))) {
+            $return = $return->where('purchase.id', '=', Request::get('id'));
+        }
+
+        if (!empty(Request::get('supplier_id'))) {
+            $return = $return->where('supplier.supplier_name', 'like', '%' . Request::get('supplier_id'));
+        }
+
+        if (!empty(Request::get('total_item'))) {
+            $return = $return->where('purchase.total_item', 'like', '%' . Request::get('total_item'));
+        }
+
+        if (!empty(Request::get('total_price'))) {
+            $return = $return->where('purchase.total_price', 'like', '%' . Request::get('total_price'));
+        }
+
+        if (!empty(Request::get('discount'))) {
+            $return = $return->where('purchase.discount', 'like', '%' . Request::get('discount'));
+        }
+        // Search end
 
         $return = $return->paginate(20);
         return $return;
