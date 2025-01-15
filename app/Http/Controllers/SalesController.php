1bdca630a8c464341f636ebe408eea765002f96b
kanayofirms <kanyeze@gmail.com>
2025-01-15 02:29:37 +0100

    feat(controller): add store method in SalesController to handle sales record creation

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MemberModel;
 use Illuminate\Http\Request;
 use App\Models\SalesModel;
+use App\Models\User;
 
 class SalesController extends Controller
 {
@@ -14,5 +16,26 @@ class SalesController extends Controller
             ->join('users', 'users.id', '=', 'sales.user_id')
             ->get();
         return view('sales.list', $data);
+    }
+
+    public function add(Request $request)
+    {
+        $data['getMember'] = MemberModel::get();
+        $data['getUser'] = User::where('is_role', '=', 2)->get();
+        return view('sales.add', $data);
+    }
+
+    public function store(Request $request)
+    {
+        $save = new SalesModel;
+        $save->member_id = trim($request->member_id);
+        $save->total_item = trim($request->total_item);
+        $save->total_price = trim($request->total_price);
+        $save->discount = trim($request->discount);
+        $save->accepted = trim($request->accepted);
+        $save->user_id = trim($request->user_id);
+        $save->save();
+
+        return redirect('admin/sales')->with('success', 'Sales successfully created.');
     }
 }
