fd35e68d91291543cef21383cc92f3cf1c6aa848
kanayofirms <kanyeze@gmail.com>
2025-01-15 02:50:03 +0100

    feat(route): add route for editing sales records by ID

--- routes/web.php
+++ routes/web.php
@@ -71,6 +71,7 @@ Route::group(['middleware' => 'admin'], function () {
     Route::get('admin/sales', [SalesController::class, 'index']);
     Route::get('admin/sales/add', [SalesController::class, 'add']);
     Route::post('admin/sales/add', [SalesController::class, 'store']);
+    Route::get('admin/sales/edit/{id}', [SalesController::class, 'edit']);
     // Sales end
 });
 
